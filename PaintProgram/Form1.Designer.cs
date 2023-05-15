897a5218e4af84cf89e669e1f2c9354569ce3d3d
Yessenia Martinez <l21211990@tectijuana.edu.mx>
2023-05-15 10:43:38 -0700

    Winneman

---
+++ PaintProgram/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace PaintProgram
+{
+    partial class Pinturitas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pinturitas));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Nuevo = new System.Windows.Forms.Button();
+            this.Guardar = new System.Windows.Forms.Button();
+            this.Cubeta = new System.Windows.Forms.Button();
+            this.Colores = new System.Windows.Forms.Button();
+            this.Borrar = new System.Windows.Forms.Button();
+            this.Circulo = new System.Windows.Forms.Button();
+            this.Rectangulo = new System.Windows.Forms.Button();
+            this.Line = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Pic = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.Nuevo);
+            this.panel1.Controls.Add(this.Guardar);
+            this.panel1.Controls.Add(this.Cubeta);
+            this.panel1.Controls.Add(this.Colores);
+            this.panel1.Controls.Add(this.Borrar);
+            this.panel1.Controls.Add(this.Circulo);
+            this.panel1.Controls.Add(this.Rectangulo);
+            this.panel1.Controls.Add(this.Line);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1132, 169);
+            this.panel1.TabIndex = 0;
+            // 
+            // Nuevo
+            // 
+            this.Nuevo.BackColor = System.Drawing.Color.Transparent;
+            this.Nuevo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Nuevo.BackgroundImage")));
+            this.Nuevo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Nuevo.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Nuevo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Nuevo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Nuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Nuevo.ForeColor = System.Drawing.Color.Transparent;
+            this.Nuevo.Location = new System.Drawing.Point(1006, 21);
+            this.Nuevo.Name = "Nuevo";
+            this.Nuevo.Size = new System.Drawing.Size(108, 110);
+            this.Nuevo.TabIndex = 8;
+            this.Nuevo.UseVisualStyleBackColor = false;
+            this.Nuevo.Click += new System.EventHandler(this.Nuevo_Click);
+            // 
+            // Guardar
+            // 
+            this.Guardar.BackColor = System.Drawing.Color.Transparent;
+            this.Guardar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Guardar.BackgroundImage")));
+            this.Guardar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Guardar.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Guardar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Guardar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Guardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Guardar.ForeColor = System.Drawing.Color.Transparent;
+            this.Guardar.Location = new System.Drawing.Point(874, 20);
+            this.Guardar.Name = "Guardar";
+            this.Guardar.Size = new System.Drawing.Size(108, 110);
+            this.Guardar.TabIndex = 7;
+            this.Guardar.UseVisualStyleBackColor = false;
+            this.Guardar.Click += new System.EventHandler(this.Guardar_Click);
+            // 
+            // Cubeta
+            // 
+            this.Cubeta.BackColor = System.Drawing.Color.Transparent;
+            this.Cubeta.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Cubeta.BackgroundImage")));
+            this.Cubeta.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Cubeta.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Cubeta.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Cubeta.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Cubeta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Cubeta.ForeColor = System.Drawing.Color.Transparent;
+            this.Cubeta.Location = new System.Drawing.Point(755, 20);
+            this.Cubeta.Name = "Cubeta";
+            this.Cubeta.Size = new System.Drawing.Size(108, 110);
+            this.Cubeta.TabIndex = 6;
+            this.Cubeta.UseVisualStyleBackColor = false;
+            this.Cubeta.Click += new System.EventHandler(this.Cubeta_Click);
+            // 
+            // Colores
+            // 
+            this.Colores.BackColor = System.Drawing.Color.Transparent;
+            this.Colores.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Colores.BackgroundImage")));
+            this.Colores.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Colores.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Colores.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Colores.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Colores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Colores.ForeColor = System.Drawing.Color.Transparent;
+            this.Colores.Location = new System.Drawing.Point(635, 20);
+            this.Colores.Name = "Colores";
+            this.Colores.Size = new System.Drawing.Size(108, 110);
+            this.Colores.TabIndex = 5;
+            this.Colores.UseVisualStyleBackColor = false;
+            this.Colores.Click += new System.EventHandler(this.Colores_Click);
+            // 
+            // Borrar
+            // 
+            this.Borrar.BackColor = System.Drawing.Color.Transparent;
+            this.Borrar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Borrar.BackgroundImage")));
+            this.Borrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Borrar.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Borrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Borrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Borrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Borrar.ForeColor = System.Drawing.Color.Transparent;
+            this.Borrar.Location = new System.Drawing.Point(513, 20);
+            this.Borrar.Name = "Borrar";
+            this.Borrar.Size = new System.Drawing.Size(108, 110);
+            this.Borrar.TabIndex = 4;
+            this.Borrar.UseVisualStyleBackColor = false;
+            this.Borrar.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Circulo
+            // 
+            this.Circulo.BackColor = System.Drawing.Color.Transparent;
+            this.Circulo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Circulo.BackgroundImage")));
+            this.Circulo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Circulo.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Circulo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Circulo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Circulo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Circulo.ForeColor = System.Drawing.Color.Transparent;
+            this.Circulo.Location = new System.Drawing.Point(386, 21);
+            this.Circulo.Name = "Circulo";
+            this.Circulo.Size = new System.Drawing.Size(108, 110);
+            this.Circulo.TabIndex = 3;
+            this.Circulo.UseVisualStyleBackColor = false;
+            this.Circulo.Click += new System.EventHandler(this.Circulo_Click);
+            // 
+            // Rectangulo
+            // 
+            this.Rectangulo.BackColor = System.Drawing.Color.Transparent;
+            this.Rectangulo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Rectangulo.BackgroundImage")));
+            this.Rectangulo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Rectangulo.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Rectangulo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Rectangulo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Rectangulo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Rectangulo.ForeColor = System.Drawing.Color.Transparent;
+            this.Rectangulo.Location = new System.Drawing.Point(260, 21);
+            this.Rectangulo.Name = "Rectangulo";
+            this.Rectangulo.Size = new System.Drawing.Size(108, 110);
+            this.Rectangulo.TabIndex = 2;
+            this.Rectangulo.UseVisualStyleBackColor = false;
+            this.Rectangulo.Click += new System.EventHandler(this.Rectangulo_Click);
+            // 
+            // Line
+            // 
+            this.Line.BackColor = System.Drawing.Color.Transparent;
+            this.Line.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Line.BackgroundImage")));
+            this.Line.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Line.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Line.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Line.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.Line.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Line.ForeColor = System.Drawing.Color.Transparent;
+            this.Line.Location = new System.Drawing.Point(136, 21);
+            this.Line.Name = "Line";
+            this.Line.Size = new System.Drawing.Size(108, 110);
+            this.Line.TabIndex = 1;
+            this.Line.UseVisualStyleBackColor = false;
+            this.Line.Click += new System.EventHandler(this.Line_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(12, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 110);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Pic
+            // 
+            this.Pic.BackColor = System.Drawing.Color.White;
+            this.Pic.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Pic.Location = new System.Drawing.Point(0, 169);
+            this.Pic.Name = "Pic";
+            this.Pic.Size = new System.Drawing.Size(1132, 576);
+            this.Pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.Pic.TabIndex = 1;
+            this.Pic.TabStop = false;
+            this.Pic.Click += new System.EventHandler(this.PicClick);
+            this.Pic.Paint += new System.Windows.Forms.PaintEventHandler(this.Pic_Paint);
+            this.Pic.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Pic_MouseClick);
+            this.Pic.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Pic_MouseDown);
+            this.Pic.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Pic_MouseMove);
+            this.Pic.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Pic_MouseUp);
+            // 
+            // Pinturitas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1132, 745);
+            this.Controls.Add(this.Pic);
+            this.Controls.Add(this.panel1);
+            this.Name = "Pinturitas";
+            this.Text = "Pinturitas";
+            this.Load += new System.EventHandler(this.Pinturitas_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox Pic;
+        private System.Windows.Forms.Button Guardar;
+        private System.Windows.Forms.Button Cubeta;
+        private System.Windows.Forms.Button Colores;
+        private System.Windows.Forms.Button Borrar;
+        private System.Windows.Forms.Button Circulo;
+        private System.Windows.Forms.Button Rectangulo;
+        private System.Windows.Forms.Button Line;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Nuevo;
+    }
+}
+
---
+++ PaintProgram/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PaintProgram
+{
+    public partial class Pinturitas : Form
+    {
+        //VARIABLES PARA DIBUJAR
+        Bitmap bmPic;
+        Graphics gPic;
+        bool paint = false;
+        Point px, py;
+
+        //objeto lapiz de color negro y grosor 1
+        Pen lapiz = new Pen(Color.Black, 1);
+
+        //objeto lapiz borrador y grosor 10
+        Pen borrador = new Pen(Color.White, 10);
+
+        //VARIABLES PARA LOS PUNTOS DEL CIRCULO
+        int x, y, sx, sy, cX, cY;
+        
+        int index;
+
+        Color NuevoColor;
+        ColorDialog PaletaColores = new ColorDialog();
+
+        //Boton Lapiz
+        private void button1_Click(object sender, EventArgs e)
+        {
+            index = 1; 
+        }
+
+        //Boton Borrar
+        private void button5_Click(object sender, EventArgs e)
+        {
+            index = 2;
+        }
+        //boton circulo
+        private void Circulo_Click(object sender, EventArgs e)
+        {
+            index = 3;
+        }
+        
+        //BOTON RECTANGULO
+        private void Rectangulo_Click(object sender, EventArgs e)
+        {
+            index = 4;
+        }
+        
+        //BOTON LINEA RECTA
+        private void Line_Click(object sender, EventArgs e)
+        {
+            index = 5;
+        }
+        //cubeta metodo
+        private void Cubeta_Click(object sender, EventArgs e)
+        {
+            index = 6;
+        }
+
+        //BORRAR TODO
+        private void Nuevo_Click(object sender, EventArgs e)
+        {
+            gPic.Clear(Color.White);
+            Pic.Image = bmPic;
+            index = 0;
+        }
+
+
+        //INICIALIZAR FORMA
+        public Pinturitas()
+        {
+            InitializeComponent();
+            bmPic = new Bitmap(Pic.Width, Pic.Height);
+            gPic = Graphics.FromImage(bmPic);
+
+            //CAMBIA EL COLOR DEL LAPIZ PARA BORRAR
+            gPic.Clear(Color.White);
+
+            Pic.Image = bmPic;
+        }
+
+        //METODO CLIC PARA EL PICTUREBOX
+        private void PicClick(object sender, EventArgs e)
+        {
+
+        }
+        
+        //PALETA DE COLORES
+        private void Colores_Click(object sender, EventArgs e)
+        {
+            PaletaColores.ShowDialog();
+            NuevoColor = PaletaColores.Color;
+            Pic.BackColor = PaletaColores.Color;
+            lapiz.Color = PaletaColores.Color;
+
+        }
+
+        //Agregar Nuevo color
+        private void Pic_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(index ==6)
+            {
+                Point punto = set_Point(Pic, e.Location);
+                Fill(bmPic, punto.X, punto.Y, NuevoColor);
+            }
+        }
+        
+        //ACCIONES DEL MOUSE HACIA ABAJO
+        private void Pic_MouseDown(object sender, MouseEventArgs e)
+        {
+            // si pinta hacia abajo
+             paint = true;
+            py = e.Location;
+
+            //DIBUJAR EL CIRCULO EN LAS DIMENSIONES X & Y
+            cX = e.X;
+            cY = e.Y;
+        }
+        //ACCIONES DEL MOUSE SE MUEVE
+        private void Pic_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (paint)
+            {
+                //para dibujar a mano alzada
+                if (index == 1)
+                {
+                    //punto x en localizacion
+                    px = e.Location;
+                    //pintar una linea tipo Glapiz
+                    gPic.DrawLine(lapiz, px, py);
+                    py = px;
+
+                }
+
+                //para borrar lo que se dibujo
+                if (index == 2)
+                {
+                    //punto x en localizacion
+                    px = e.Location;
+                    //pintar una linea tipo PEN+BORRADOR
+                    gPic.DrawLine(borrador, px, py);
+                    py = px;
+
+                }
+
+
+                
+            }                
+            Pic.Refresh();
+            
+            //variables para el ciculo 
+                x = e.X;
+                y = e.Y;
+                sx = cX - cX;
+                sy = cY - cY;
+        }
+
+        //ACCIONES DEL MOUSE HACIA ARRIBA
+        private void Pic_MouseUp(object sender, MouseEventArgs e)
+        {
+            paint = false;
+
+            sx = x - cX;
+            sy = y - cY;
+
+            //PARA DIBUJAR EL CIRCULO
+            if (index == 3)
+            {
+                gPic.DrawEllipse(lapiz, cX, cY, sx, sy);
+            }
+
+            //PARA DIBUJAR EL RECTANGULO
+            if (index == 4)
+            {
+                gPic.DrawRectangle(lapiz, cX, cY, sx, sy);
+            }
+
+            //PARA DIBUJAR LINEA RECTA
+            if (index == 5)
+            {
+                gPic.DrawLine(lapiz, cX, cY, x, y);
+            }
+
+        }
+
+
+
+        
+        //BOTON GUARDAR
+        private void Guardar_Click(object sender, EventArgs e)
+        {
+            var GuardarDoc = new SaveFileDialog();
+            GuardarDoc.Filter = "Image(*.jpg)|*.jpg|(*.*)|*.*";
+            if(GuardarDoc.ShowDialog()== DialogResult.OK)
+            {
+                Bitmap docSave = bmPic.Clone(new Rectangle(0, 0, Pic.Width, Pic.Height), bmPic.PixelFormat);
+                docSave.Save(GuardarDoc.FileName, ImageFormat.Jpeg);
+
+            }
+        }
+
+        private void Pinturitas_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //para pintar las figuras al tama;o que deseas
+        private void Pic_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gPic = e.Graphics;
+            if (paint)
+            {
+                if (index == 3)
+                {
+                    gPic.DrawEllipse(lapiz, cX, cY, sx, sy);
+                }
+
+                //PARA DIBUJAR EL RECTANGULO
+                if (index == 4)
+                {
+                    gPic.DrawRectangle(lapiz, cX, cY, sx, sy);
+                }
+
+                
+            }
+        }
+
+
+        //CUBETA
+        static Point set_Point(PictureBox cajita, Point punto)
+        {
+            float px = 1f * cajita.Width / cajita.Width;
+            float py = 1f * cajita.Height / cajita.Height;
+            return new Point((int)(punto.X * px), (int)(punto.Y * py));
+        }
+
+        //validacion
+        private void Validate(Bitmap bmValdate, Stack<Point> StackPoint, int x, int y, Color Old_Color, Color nuevoColor)
+        {
+            Color ColorX = bmValdate.GetPixel(x, y);
+            if (ColorX == Old_Color)
+            {
+
+                StackPoint.Push(new Point(x, y));
+                bmValdate.SetPixel(x, y, nuevoColor);
+            }
+
+        }
+
+        //cubeta para pintar
+        public void Fill(Bitmap bmPic, int x, int y, Color New_Color)
+        {
+            Color Old_Color = bmPic.GetPixel(x, y);
+            Stack<Point> pixel = new Stack<Point>();
+            pixel.Push(new Point(x, y));
+            bmPic.SetPixel(x, y, New_Color);
+
+            if (Old_Color == New_Color)
+            {
+                return;
+            }
+
+            while (pixel.Count > 0)
+            {
+                Point puntoNuevo = (Point)pixel.Pop();
+                if (puntoNuevo.X > 0 && puntoNuevo.Y > 0 && puntoNuevo.X < bmPic.Width - 1 && puntoNuevo.Y < bmPic.Height - 1)
+                {
+                    Validate(bmPic, pixel, puntoNuevo.X - 1, puntoNuevo.Y, Old_Color, New_Color);
+                    Validate(bmPic, pixel, puntoNuevo.X, puntoNuevo.Y - 1, Old_Color, New_Color);
+                    Validate(bmPic, pixel, puntoNuevo.X + 1, puntoNuevo.Y, Old_Color, New_Color);
+                    Validate(bmPic, pixel, puntoNuevo.X, puntoNuevo.Y + 1, Old_Color, New_Color);
+                }
+            }
+        }
+
+
+    }
+}
